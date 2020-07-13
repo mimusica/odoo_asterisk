f88dc347e6393d75d5bfe26f4fc48b5ba2a2366c
Christophe Langenberg <christophe@langenberg.be>
2020-07-13 13:00:15 +0200

    created the scaffold for asterisk_conf_mngmnt

--- odoo/addons/asterisk_conf_mngmnt/__manifest__.py
+++ odoo/addons/asterisk_conf_mngmnt/__manifest__.py
@@ -1,21 +1,37 @@
+# -*- coding: utf-8 -*-
 {
-    'name': 'Odoo Asterisk configuration management',
-    'summary': 'base module for Asterisk management',
-    'description':
-        """
+    'name': "Odoo Asterisk configuration manager",
+
+    'summary': """
+        base module for working with Asterisk modules created by the author of this module""",
+
+    'description': """
         This module let's you manage and configure your remote Asterisk PBX's
-        """,
-    'version': '0.0.1',
+    """,
+
+    'author': "Christophe Langenberg",
+    'website': "https://christophe.langenberg.be",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
     'category': 'Telephony',
-    'author': 'Christophe Langenberg',
-    'website': 'http://christophe.langenberg.be',
-    'depends': ['web', 'bus'],
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'web', 'bus'],
+
+    # always loaded
     'data': [
-        'views/menu.xml',
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
     ],
+    # only loaded in demonstration mode
     'demo': [
+        'demo/demo.xml',
     ],
     'qweb': [
-        'static/src/xml/*.xml',
-    ],
+            'static/src/xml/*.xml',
+        ],
 }
